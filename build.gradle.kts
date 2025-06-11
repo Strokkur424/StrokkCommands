e5e67065212b926cf9aea5f872236f6153f54192
Strokkur424 <strokkur.24@gmail.com>
2025-06-11 21:40:08 +0200

    Add licensing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,32 @@
+import net.kyori.indra.licenser.spotless.IndraSpotlessLicenserExtension
+
 plugins {
     id("java-library")
+    id("net.kyori.indra.licenser.spotless") version "3.1.3"
 }
 
 group = "net.strokkur"
 version = "1.2.0"
 
+fun IndraSpotlessLicenserExtension.apply() {
+    licenseHeaderFile(rootProject.file("HEADER"))
+}
+
 subprojects {
     plugins.apply("java-library")
-    
+    plugins.apply("net.kyori.indra.licenser.spotless")
+
     repositories {
         mavenCentral()
         mavenLocal()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
+
+    indraSpotlessLicenser.apply()
 }
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
+
+indraSpotlessLicenser.apply()
--- strokk-commands-test-plugin/src/main/java/net/strokkur/testplugin/TestPlugin.java
+++ strokk-commands-test-plugin/src/main/java/net/strokkur/testplugin/TestPlugin.java
@@ -1,18 +1,35 @@
+/*
+ * StrokkCommands - A super simple annotation based zero-shade Paper command API library.
+ * Copyright (C) 2025 Strokkur24
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <https://www.gnu.org/licenses/>.
+ */
 package net.strokkur.testplugin;
 
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-//import net.strokkur.testplugin.commands.AdventureArgumentsCommandBrigadier;
-//import net.strokkur.testplugin.commands.EntitiesCommandBrigadier;
-//import net.strokkur.testplugin.commands.LiteralsCommandBrigadier;
-//import net.strokkur.testplugin.commands.LocationArgumentsCommandBrigadier;
-//import net.strokkur.testplugin.commands.PaperArgumentsCommandBrigadier;
-//import net.strokkur.testplugin.commands.PredicateArgumentsCommandBrigadier;
-//import net.strokkur.testplugin.commands.PrimitivesCommandBrigadier;
-//import net.strokkur.testplugin.commands.RegistryArgumentsCommandBrigadier;
-//import net.strokkur.testplugin.commands.TellMiniCommandBrigadier;
-//import net.strokkur.testplugin.iceacream.IceCreamCommandBrigadier;
-//import net.strokkur.testplugin.suggestions.CommandWithSuggestionsBrigadier;
+import net.strokkur.testplugin.commands.AdventureArgumentsCommandBrigadier;
+import net.strokkur.testplugin.commands.EntitiesCommandBrigadier;
+import net.strokkur.testplugin.commands.LiteralsCommandBrigadier;
+import net.strokkur.testplugin.commands.LocationArgumentsCommandBrigadier;
+import net.strokkur.testplugin.commands.PaperArgumentsCommandBrigadier;
+import net.strokkur.testplugin.commands.PredicateArgumentsCommandBrigadier;
+import net.strokkur.testplugin.commands.PrimitivesCommandBrigadier;
+import net.strokkur.testplugin.commands.RegistryArgumentsCommandBrigadier;
+import net.strokkur.testplugin.commands.TellMiniCommandBrigadier;
+import net.strokkur.testplugin.iceacream.IceCreamCommandBrigadier;
+import net.strokkur.testplugin.suggestions.CommandWithSuggestionsBrigadier;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("UnstableApiUsage")
@@ -22,19 +39,19 @@ public final class TestPlugin extends JavaPlugin {
     public void onLoad() {
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(event -> {
             Commands commands = event.registrar();
-            
-//            CommandWithSuggestionsBrigadier.register(commands);
-//            IceCreamCommandBrigadier.register(commands);
-//
-//            PrimitivesCommandBrigadier.register(commands);
-//            LocationArgumentsCommandBrigadier.register(commands);
-//            EntitiesCommandBrigadier.register(commands);
-//            RegistryArgumentsCommandBrigadier.register(commands);
-//            PaperArgumentsCommandBrigadier.register(commands);
-//            PredicateArgumentsCommandBrigadier.register(commands);
-//            AdventureArgumentsCommandBrigadier.register(commands);
-//            LiteralsCommandBrigadier.register(commands);
-//            TellMiniCommandBrigadier.register(commands);
+
+            CommandWithSuggestionsBrigadier.register(commands);
+            IceCreamCommandBrigadier.register(commands);
+
+            PrimitivesCommandBrigadier.register(commands);
+            LocationArgumentsCommandBrigadier.register(commands);
+            EntitiesCommandBrigadier.register(commands);
+            RegistryArgumentsCommandBrigadier.register(commands);
+            PaperArgumentsCommandBrigadier.register(commands);
+            PredicateArgumentsCommandBrigadier.register(commands);
+            AdventureArgumentsCommandBrigadier.register(commands);
+            LiteralsCommandBrigadier.register(commands);
+            TellMiniCommandBrigadier.register(commands);
         }));
     }
 }
