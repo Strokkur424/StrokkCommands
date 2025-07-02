fed7d47b7012c60252d9170a6536a2ab3271da8a
Strokkur424 <strokkur.24@gmail.com>
2025-07-02 23:50:29 +0200

    Add dependency page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+import codeConstantsPlugin from "./src/utils/remark/code_const";
+import {LATEST_COMMANDS_RELEASE, LATEST_MC_RELEASE, LATEST_PAPER_RELEASE} from "./src/utils/versions.js";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'StrokkCommands',
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/Strokkur424/StrokkCommands'}],
+            sidebar: [
+                {
+                    label: "Documentation",
+                    items: [
+                        { slug: "docs" },
+                        { slug: "docs/adding-the-dependency" }
+                    ]
+                }
+            ],
+        }),
+    ],
+    markdown: {
+        remarkPlugins: [
+            [
+                codeConstantsPlugin,
+                {
+                    constants: {
+                        LATEST_MC_RELEASE,
+                        LATEST_PAPER_RELEASE,
+                        LATEST_COMMANDS_RELEASE
+                    }
+                }
+            ]
+        ]
+    }
 });
