c6881b9ac5ec54f87aa50c30c65a3b5cd5e2e721
Strokkur424 <strokkur.24@gmail.com>
2025-07-05 14:26:19 +0200

    Add arguments.mdx

---
+++ strokk-commands-test-plugin/src/main/java/net/strokkur/testplugin/docs/TellMiniCommand.java
@@ -0,0 +1,40 @@
+/*
+ * StrokkCommands - A super simple annotation based zero-shade Paper command API library.
+ * Copyright (C) 2025 Strokkur24
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <https://www.gnu.org/licenses/>.
+ */
+package net.strokkur.testplugin.docs;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.strokkur.commands.annotations.Command;
+import net.strokkur.commands.annotations.Executes;
+import net.strokkur.commands.annotations.arguments.StringArg;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+import static net.strokkur.commands.StringArgType.GREEDY;
+
+@Command("tellmini")
+public class TellMiniCommand {
+    
+    @Executes
+    void tellMini(CommandSender sender, List<Player> players, @StringArg(GREEDY) String message) {
+        Component component = MiniMessage.miniMessage().deserialize(message);
+        players.forEach(p -> p.sendMessage(component));
+    }
+}
