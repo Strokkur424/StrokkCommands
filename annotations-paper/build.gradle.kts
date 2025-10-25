86db291666124a729468391b18e9056316c7f311
Strokkur24 <strokkur.24@gmail.com>
2025-10-25 10:00:29 +0200

    fix: cursed Gradle setup

--- annotations-paper/build.gradle.kts
+++ annotations-paper/build.gradle.kts
@@ -1,25 +1,4 @@
-val commonAnnotations = project(":commands-annotations-common");
-val commonAnnotationsSource = commonAnnotations.sourceSets.main.get()
-
 dependencies {
-  compileOnly(commonAnnotations)
+  api(project(":commands-annotations-common"))
   compileOnly(libs.paper.api)
-}
-
-tasks {
-  build {
-    dependsOn(commonAnnotations.tasks.build)
-  }
-
-  jar {
-    from(commonAnnotationsSource.output)
-  }
-
-  sourcesJar {
-    from(commonAnnotationsSource.allSource)
-  }
-
-  javadocJar {
-    from(commonAnnotationsSource.allSource)
-  }
 }
