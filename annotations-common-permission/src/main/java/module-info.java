7566078d12dfef449e55d3ae43272ebedc3f1f98
Strokkur24 <strokkur.24@gmail.com>
2025-11-02 13:03:50 +0100

    style: rename permission module

--- annotations-common-permission/src/main/java/module-info.java
+++ annotations-common-permission/src/main/java/module-info.java
@@ -1,4 +1,4 @@
 /// A common module for all platforms which have some sort of string-based permission system.
-module net.strokkur.commands.permission {
+module net.strokkur.commands.common.permission {
   exports net.strokkur.commands.permission;
 }
--- annotations-velocity/src/main/java/module-info.java
+++ annotations-velocity/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /// Annotations specific to the Velocity platform.
 module net.strokkur.commands.velocity {
   requires static transitive net.strokkur.commands.common;
-  requires static transitive net.strokkur.commands.permission;
+  requires static transitive net.strokkur.commands.common.permission;
 }
