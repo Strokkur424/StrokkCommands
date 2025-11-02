f8ae1ac136770031c78bf3c65165d78f67ba6c30
Strokkur24 <strokkur.24@gmail.com>
2025-11-02 13:02:34 +0100

    chore: add javadoc comments to module-info.java files

--- annotations-common-permission/src/main/java/module-info.java
+++ annotations-common-permission/src/main/java/module-info.java
@@ -1,3 +1,4 @@
+/// A common module for all platforms which have some sort of string-based permission system.
 module net.strokkur.commands.permission {
   exports net.strokkur.commands.permission;
 }
